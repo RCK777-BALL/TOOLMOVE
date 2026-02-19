d6ab54564bbb74f262dd9e1c80c9e50bd74a8bc5
jamerrig <jamerrig@gmail.com>
2026-02-18 21:48:15 -0500

    CORS Error fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,13 @@ import seedRoutes from "./routes/seedRoutes.js";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({
+  origin: [
+    process.env.CORS_ORIGIN
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(morgan("dev"));
 
